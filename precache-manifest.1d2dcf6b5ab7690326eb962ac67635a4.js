d848dc804507b61396f08cb00a8538500ecacf8c
jobi <olajobinurudeen2013@gmail.com>
2020-04-30 16:16:35 +0100

    Updates

---
+++ precache-manifest.1d2dcf6b5ab7690326eb962ac67635a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b1889fda3b7c2cfa8219ac5415444f",
+    "url": "/devsFriends/index.html"
+  },
+  {
+    "revision": "03fe53a2b63cab73a476",
+    "url": "/devsFriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "efa7fe8ebda2354e0c77",
+    "url": "/devsFriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "03fe53a2b63cab73a476",
+    "url": "/devsFriends/static/js/2.289be98e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/devsFriends/static/js/2.289be98e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa7fe8ebda2354e0c77",
+    "url": "/devsFriends/static/js/main.4396ffa3.chunk.js"
+  },
+  {
+    "revision": "3da9b1e681117fb98317",
+    "url": "/devsFriends/static/js/runtime-main.6abeba7e.js"
+  }
+]);
